d237bfb9f1ccc0d135c1778f8358cc96c36a0709
Shazeen Fabius <Shazeen-Fabius@ShazeenabiusMBP.lan>
2020-10-14 09:11:51 -0700

    created the form file for the layout

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './Form';
 
 function App() {
+  const [names, setNames] = useState([
+    {
+      id: 1,
+      name: "Shazeen",
+      email: "Fabius",
+      role: "Student"
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Team Names</h1>
+      <Form names={names}/>
     </div>
   );
 }
---
+++ team-builder/src/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+
+function Form(props) {
+    const {names} = props;
+    console.log(names);
+    return (
+        <div>
+            <h1>Form</h1>
+            {names.map((names) => (
+                    <div key={names.id} className='teamNames'>
+                        <p>{names.name}</p>
+                        <p>{names.email}</p>
+                        <p>{names.role}</p>
+                    </div>
+            ))}
+        </div>
+    );
+}
+
+export default Form;
